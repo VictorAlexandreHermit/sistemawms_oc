f2ccc0f86a4365f187c80500c720abf1465027fd
Victor Alexandre <vsantosrj2001@gmail.com>
2026-09-13 20:17:03 -0300

    fix: cache-busting nos assets app.css/app.js (v=20260913) para garantir refresh pos-deploy

--- app/views/templates/layout_interno.php
+++ app/views/templates/layout_interno.php
@@ -17,7 +17,7 @@ $rotaBase = explode('/', $rotaAtiva)[0];
     <meta name="robots" content="noindex, nofollow">
     <title><?php echo SecurityHelper::e($titulo ?? 'WMS Agiliza'); ?> · WMS Agiliza</title>
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/app.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/app.css?v=20260913">
 </head>
 <body>
 <div class="wms-layout">
@@ -111,6 +111,6 @@ $rotaBase = explode('/', $rotaAtiva)[0];
 </div>
 
 <script src="<?php echo BASE_URL; ?>/assets/js/bootstrap.bundle.min.js"></script>
-<script src="<?php echo BASE_URL; ?>/assets/js/app.js"></script>
+<script src="<?php echo BASE_URL; ?>/assets/js/app.js?v=20260913"></script>
 </body>
 </html>
